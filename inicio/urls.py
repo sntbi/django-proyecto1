2411fedca620558cefca2cd579f5ccd34eef54ee
sntbi <santibis2808@gmail.com>
2024-07-03 18:37:52 -0300

    cambie la direccion de los urls de inicio desde miproyecto a inicio

---
+++ inicio/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from inicio import views
+
+urlpatterns = [
+    path('', views.inicio),
+    path('template1/<str:nombre>/<str:apellido>/<edad>/', views.template1),
+    path('template2/<str:nombre>/<str:apellido>/<edad>/', views.template2),
+    path('template3/<str:nombre>/<str:apellido>/<edad>/', views.template3),
+    path('template4/<str:nombre>/<str:apellido>/<edad>/', views.template4),
+    path('probando/', views.probando),
+]
