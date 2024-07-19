aedaca0c00a983b364e7e81c05bfc7cb930395d6
santiagobisceglia <santibis2808@gmail.com>
2024-07-19 15:23:03 -0300

    cree las vistas de editar, eliminar zapatillas

--- producto/urls.py
+++ producto/urls.py
@@ -7,10 +7,12 @@
 
 
 from django.urls import path
-from .views import ZapatillasView, CrearZapatilla, VerZapatilla
+from .views import ZapatillasView, CrearZapatilla, VerZapatilla, EditarZapatilla, EliminarZapatilla
 
 urlpatterns = [
     path('zapatilla/', ZapatillasView.as_view(), name='zapatillas'),
     path('zapatilla/crear', CrearZapatilla.as_view(), name='crear_zapatilla'),
     path('zapatilla/<int:pk>', VerZapatilla.as_view(), name='ver_zapatilla'),
+    path('zapatilla/<int:pk>/editar/', EditarZapatilla.as_view(), name='editar_zapatilla'),
+    path('zapatilla/<int:pk>/borrar/', EliminarZapatilla.as_view(), name='eliminar_zapatilla'),
 ]
--- producto/views.py
+++ producto/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from .models import Zapatilla
 from django.urls import reverse_lazy
@@ -24,8 +24,16 @@ class CrearZapatilla(CreateView):
     fields = ['marca', 'descripcion', 'fecha']
     
 class EditarZapatilla(UpdateView):
-    ...
+    model = Zapatilla
+    template_name = 'zapatillas/editar_zapatilla.html'
+    success_url = reverse_lazy('zapatillas')
+    fields = ['marca', 'descripcion', 'fecha']
     
 class VerZapatilla(DetailView):
     model = Zapatilla
     template_name = 'zapatillas/ver_zapatilla.html'
+    
+class EliminarZapatilla(DeleteView):
+    model = Zapatilla
+    template_name = "zapatillas/eliminar_zapatilla.html"
+    success_url = reverse_lazy('zapatillas')
