24074bbff88975682c4ad67d846e1535d1866b88
santiagobisceglia <santibis2808@gmail.com>
2024-07-22 19:15:36 -0300

    cree usuarios

--- miproyecto/urls.py
+++ miproyecto/urls.py
@@ -6,5 +6,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('inicio.urls')),
-    path('producto/', include('producto.urls'))
+    path('producto/', include('producto.urls')),
+    path('usuarios/', include('usuarios.urls')),
 ]
